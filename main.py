bd78c6df57866215e3c0e8fa211132177a471191
20012345 <20012345@tafe.wa.edu.au>
2025-06-30 12:20:52 +0800

    style: substitute sad_blink img

--- main.py
+++ main.py
@@ -3,21 +3,16 @@ If you have access to a SenseHAT (either via a Raspberry Pi or a SenseHAT emulat
 That is, delete the sense_hat.py file that is included in this bundle."""
 
 import time
-
-# from happy import Happy  # Uncomment for Happy class and comment the Sad import/class.
-from sad import Sad  # Swap with Happy for the Happy class.
+from sad import Sad  # Or switch to happy/Happy if needed
 
 def main():
-
-  #  smiley = Happy()  # Uncomment for Happy class and comment the Sad import/class.
-
-    smiley = Sad()  # Swap with Happy for the Happy class.
+    smiley = Sad()  # Change to Happy() if testing Happy class
 
     smiley.show()
 
     time.sleep(1)
 
-    smiley.blink()
+    smiley.blink(delay=0.5)
 
 if __name__ == '__main__':
     ############################################################
